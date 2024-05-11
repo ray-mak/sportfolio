f5f87b1e96dcee6cf99bed38958df4bd7be3ea4a
Ray Mak <projectraymak@gmail.com>
2024-05-10 17:16:19 -0700

    Updated handleProbability and added Profit amount
    Previous handleProbability function was not converting to the correct American and decimals odds. Also added a profit amount function

--- frontend/src/features/bets/AddPick.jsx
+++ frontend/src/features/bets/AddPick.jsx
@@ -122,7 +122,7 @@ const AddPick = () => {
         }
 
     }
-    console.log(americanOdds, decimalOdds, probability)
+
 
     function handleDecimalOddsChange(e) {
         const newDecimalOdds = e.target.value
@@ -145,21 +145,59 @@ const AddPick = () => {
         setProbability(newProbability)
 
         if (newProbability >= 50) {
-            const newAmericanOdds = (newProbability / (100 - newProbability)) * 100
+            const newAmericanOdds = -(newProbability / (100 - newProbability)) * 100
             const newDecimalOdds = 100 / newProbability
             setAmericanOdds(newAmericanOdds.toFixed(0))
             setDecimalOdds(newDecimalOdds.toFixed(2))
         } else {
-            const newAmericanOdds = -((100 - newProbability) / newProbability) * 100
-            const newDecimalOdds = (100 - newProbability) / newProbability
+            const newAmericanOdds = ((100 - newProbability) / newProbability) * 100
+            const newDecimalOdds = (100 - newProbability) / newProbability + 1
             setAmericanOdds(newAmericanOdds.toFixed(0))
             setDecimalOdds(newDecimalOdds.toFixed(2))
         }
     }
 
+    //handle and store bet amount
+    const [betAmount, setBetAmount] = useState("")
+    function handleBetAmount(e) {
+        let betInput = e.target.value
+        if (betInput.length > 4) {
+            betInput = betInput.slice(0, 4)
+        }
+        if (betInput < 0) {
+            betInput = 0
+        } else if (betInput > 10) {
+            betInput = 10
+        }
+        setBetAmount(betInput)
+    }
+
+    //calculate profit amount
+    const [profitAmount, setProfitAmount] = useState(0)
+    useEffect(() => {
+        if (betAmount && decimalOdds) {
+            const profit = betAmount * decimalOdds - betAmount
+            setProfitAmount(profit.toFixed(2))
+        } else {
+            setProfitAmount(0)
+        }
+    }, [betAmount, decimalOdds])
+
+    //handle and store notes
+    const [notes, setNotes] = useState("")
+    function handleNotesChange(e) {
+        setNotes(e.target.value)
+    }
+
+    //handle submit
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+    console.log(americanOdds, decimalOdds, probability, betAmount)
     return (
-        <div className='flex flex-col items-center'>
-            <form className='flex flex-col w-5/6 gap-6 border-2 p-4 lg:w-3/5 2xl:w-1/2'>
+        <div className='h-screen flex flex-col items-center justify-center text-sm sm:text-base'>
+            <form onSubmit={handleSubmit} className='flex flex-col w-full gap-6 border-2 p-4 sm:w-5/6 lg:w-3/5 2xl:w-1/2'>
                 <div ref={eventContainer}>
                     <p>Event</p>
                     <div className='relative'>
@@ -189,23 +227,21 @@ const AddPick = () => {
                         </div>}
                     </div>
                 </div>
-                <div ref={pickContainer}>
+                <div ref={pickContainer} className='relative'>
                     <p>Pick</p>
-                    <div className='relative'>
-                        <div className='flex items-center justify-between bg-white px-4 py-2 rounded-lg mt-1 hover:cursor-pointer' aria-label='Select matchup' onClick={togglePickDropdown}>
-                            <span>{pickedFighter}</span>
-                            <FontAwesomeIcon icon={faChevronDown} />
-                        </div>
-                        {pickDropdown && <div className='w-full mt-2 rounded-lg bg-white  p-2 dropdown absolute'>
-                            <ul className='options max-h-60 overflow-y-auto'>
-                                {pickList}
-                            </ul>
-                        </div>}
+                    <div className='flex items-center justify-between bg-white px-4 py-2 rounded-lg mt-1 hover:cursor-pointer' aria-label='Select matchup' onClick={togglePickDropdown}>
+                        <span>{pickedFighter}</span>
+                        <FontAwesomeIcon icon={faChevronDown} />
                     </div>
+                    {pickDropdown && <div className='w-full mt-2 rounded-lg bg-white  p-2 dropdown absolute'>
+                        <ul className='options max-h-60 overflow-y-auto'>
+                            {pickList}
+                        </ul>
+                    </div>}
                 </div>
                 <div>
                     <p>Odds</p>
-                    <div className='w-full grid grid-cols-3 gap-6'>
+                    <div className='w-full grid grid-cols-3 gap-2 sm:gap-4 md:gap-6 lg:gap-10'>
                         <label htmlFor='americanOdds'>
                             American
                             <input
@@ -236,12 +272,46 @@ const AddPick = () => {
                                     onChange={handleProbabilityChange}
                                     className='w-full px-4 py-2 rounded-lg'
                                 />
-                                <p className='text-2xl'>%</p>
+                                <p className='text-xl'>%</p>
                             </div>
                         </label>
                     </div>
                 </div>
+                <div className='grid grid-cols-2 gap-12'>
+                    <label htmlFor='betAmount'>
+                        <p>Bet [0-10] Units</p>
+                        <input
+                            id='betAmount'
+                            type='number'
+                            value={betAmount}
+                            onChange={handleBetAmount}
+                            className='w-full px-4 py-2 rounded-lg mt-1'
+                            maxLength={4}
+                            min={0}
+                            max={10}
+                        />
+                    </label>
+                    <div className='flex flex-col'>
+                        <p>To Profit</p>
+                        <p className='py-3'><span className='font-semibold'>{profitAmount}</span> units</p>
+                    </div>
+                </div>
+                <label htmlFor='notes'>
+                    <p>Notes (optional)</p>
+                    <textarea
+                        id='notes'
+                        name='notes'
+                        value={notes}
+                        onChange={handleNotesChange}
+                        className='w-full h-28 px-4 py-2 rounded-lg mt-1 resize-none'
+                    />
+                </label>
+                <button className='w-32 py-2 text-white bg-brightRed rounded-md'>Submit</button>
             </form>
+
+            <div className='absolute border-2'>
+                <p>Confirm your pick</p>
+            </div>
         </div>
     )
 }
