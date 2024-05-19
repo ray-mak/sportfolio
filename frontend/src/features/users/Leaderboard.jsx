58952387fa46afd8caf4b8a7c21b25f3417cfab2
Ray Mak <projectraymak@gmail.com>
2024-05-19 15:34:39 -0700

    Created sort function for Leaderboard
    MMALeaderboard will no longer be used. All logic is handled inside the Leaderboard component.
    This is mainly due to the fact that MMALeaderboard was used to fetch individual users. However, with the sort function, we need all user information first. So it makes more sense to fetch all the user data inside Leaderboard and then calculate/sort their stats.

--- frontend/src/features/users/Leaderboard.jsx
+++ frontend/src/features/users/Leaderboard.jsx
@@ -1,11 +1,27 @@
-import { useGetUsersQuery } from "./usersApiSlice"
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { selectUserById, useGetUsersQuery } from "./usersApiSlice"
 import MMALeaderboardUser from "./MMALeaderboardUser"
 
 //this is a users list, but this will be used for the leaderboard, which will have the users username and profits and additional stats
 const Leaderboard = () => {
-    // we rename data to users
+    //create state to store our sort values.
+    const [sort, setSort] = useState({ key: "totalProfit", direction: "desc" })
+
+    function handleSort(header) {
+        setSort({
+            key: header,
+            direction:  //check if header clicked is the same as key, if so flip direction
+                header === sort.key
+                    ? sort.direction === "asc"
+                        ? "desc"
+                        : "asc"
+                    : "desc"
+        })
+    }
+
     const {
-        data: users,
+        data,
         isLoading,
         isSuccess,
         isError,
@@ -21,12 +37,48 @@ const Leaderboard = () => {
     }
 
     if (isSuccess) {
-        //ids is an array of user ids
-        const { ids } = users
+        //enities is an object within data, this is done to convert it to an array.
+        const entities = Object.values(data.entities)
 
-        const tableContent = ids?.length
-            ? ids.map(userId => <MMALeaderboardUser key={userId} userId={userId} />)
-            : null
+        //userStats will be an array of objects that contains the users name and betting stats.
+        const userStats = []
+        entities.forEach(object => {
+            const results = object.bets
+            let picks = 0
+            let profit = 0
+            let unitsBet = 0
+            let roi
+
+            for (const result of results) {
+                picks++
+                profit = profit += Number(result.profit)
+                unitsBet = unitsBet += result.betAmount
+            }
+
+            roi = ((profit / unitsBet) * 100)
+            if (!isNaN(roi)) {
+                userStats.push({ "displayName": object.displayName, "roi": roi.toFixed(0), "totalProfit": profit.toFixed(2), "unitsBet": unitsBet, "totalPicks": picks })
+            }
+        })
+
+        function sortUsers(array) {
+            if (sort.direction === "asc") {
+                return array.sort((a, b) => (a[sort.key] - b[sort.key]))
+            } else {
+                return array.sort((a, b) => (b[sort.key] - a[sort.key]))
+            }
+        }
+
+        const tableContent = sortUsers(userStats).map((user, index) => (
+            <tr className="bg-lightGray" key={index}>
+                <td data-rank className="px-4 py-2">{index + 1}</td>
+                <td data-username className="px-4 py-2">{user.displayName}</td>
+                <td data-roi className="px-4 py-2">{user.roi}%</td>
+                <td data-units-profit className="px-4 py-2">{user.totalProfit}</td>
+                <td data-units-bet className="px-4 py-2">{user.unitsBet}</td>
+                <td data-picks className="px-4 py-2">{user.totalPicks}</td>
+            </tr>
+        ))
 
         content = (
             <div className="flex justify-center">
@@ -37,10 +89,10 @@ const Leaderboard = () => {
                             <tr className="bg-slate-700">
                                 <th scope="col" className="px-4 py-2 text-left text-white">Rank</th>
                                 <th scope="col" className="px-4 py-2 text-left text-white">User</th>
-                                <th scope="col" className="px-4 py-2 text-left text-white">ROI</th>
-                                <th scope="col" className="px-4 py-2 text-left text-white">Units Profit</th>
-                                <th scope="col" className="px-4 py-2 text-left text-white">Units Bet</th>
-                                <th scope="col" className="px-4 py-2 text-left text-white">Picks</th>
+                                <th scope="col" className="px-4 py-2 text-left text-white cursor-pointer" onClick={() => handleSort("roi")}>ROI</th>
+                                <th scope="col" className="px-4 py-2 text-left text-white cursor-pointer" onClick={() => handleSort("unitsProfit")}>Units Profit</th>
+                                <th scope="col" className="px-4 py-2 text-left text-white cursor-pointer" onClick={() => handleSort("unitsBet")}>Units Bet</th>
+                                <th scope="col" className="px-4 py-2 text-left text-white cursor-pointer" onClick={() => handleSort("totalPicks")}>Total Picks</th>
                             </tr>
                         </thead>
                         <tbody>
