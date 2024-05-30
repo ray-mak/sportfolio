c7a8c0fd753f4f13e65ba446efc2a060a264d8d7
Ray Mak <projectraymak@gmail.com>
2024-05-30 15:13:17 -0700

    Created mmaResultsApiSlice
    Created mmaResultsApiSlice to handle creating and updating mma event results. Also added event totals and styling to UserMMAEvents

---
+++ frontend/src/features/events/mmaResultsApiSlice.jsx
@@ -0,0 +1,73 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit"
+import { apiSlice } from "../../app/api/apiSlice"
+
+const mmaResultsAdapter = createEntityAdapter({})
+const initialState = mmaResultsAdapter.getInitialState()
+
+export const mmaResultsApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getMMAResults: builder.query({
+            query: () => '/api/eventresults',
+            validateStatus: (response, result) => {
+                return response.status === 200 && !result.isError
+            },
+            transformResponse: responseData => {
+                const loadedEvents = responseData.map(event => {
+                    event.id = event._id
+                    return event
+                });
+                return mmaResultsAdapter.setAll(initialState, loadedEvents)
+            },
+            providesTags: (result, error, arg) => {
+                if (result?.ids) {
+                    return [
+                        { type: 'MMAResults', id: 'LIST' },
+                        ...result.ids.map(id => ({ type: 'MMAResults', id }))
+                    ]
+                } else return [{ type: 'MMAResults', id: 'LIST' }]
+            }
+        }),
+        addNewMMAResult: builder.mutation({
+            query: initialUserData => ({
+                url: '/api/eventresults',
+                method: 'POST',
+                body: {
+                    ...initialUserData
+                }
+            }),
+            invalidatesTags: [
+                { type: 'MMAResults', id: "LIST" }
+            ]
+        }),
+        updateMMAResult: builder.mutation({
+            query: initialUserData => ({
+                url: '/api/eventresults',
+                method: "PATCH",
+                body: {
+                    ...initialUserData
+                }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: "MMAResults", id: arg.id }
+            ]
+        }),
+        deleteMMAResult: builder.mutation({
+            query: ({ id }) => ({
+                url: "/api/eventresults",
+                method: "DELETE",
+                body: { id }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: "MMAResults", id: arg.id }
+            ]
+        })
+    })
+})
+
+export const {
+    useGetMMAResultsQuery,
+    useAddNewMMAResultMutation,
+    useUpdateMMAResultMutation,
+    useDeleteMMAResultMutation
+} = mmaResultsApiSlice
+
--- frontend/src/features/users/UserMMAEvents.jsx
+++ frontend/src/features/users/UserMMAEvents.jsx
@@ -23,29 +23,65 @@ const UserMMAEvents = () => {
     if (isSuccess) {
         const betHistory = data.betHistory.map(object => {
             const betResults = object.eventResults.map(bet => {
+                let textColor
+                if (bet.result === "Win") {
+                    textColor = "text-green-700"
+                } else if (bet.result === "Loss") {
+                    textColor = "text-red-600"
+                } else {
+                    textColor = "text-gray-500"
+                }
                 return (
                     <tr key={bet._id}>
-                        <td>{bet.matchup}</td>
+                        <td className="px-4 py-1">{bet.matchup}</td>
                         <td>{bet.pick}</td>
-                        <td>{bet.result}</td>
+                        <td className={textColor}>{bet.result}</td>
                         <td>{bet.betAmount.toFixed(2)}</td>
                         <td>{bet.profit}</td>
                         <td>{bet.roi}%</td>
                     </tr>
                 )
             })
+            let totalBetAmount = 0
+            let totalProfit = 0
+            object.eventResults.forEach(bet => totalBetAmount += bet.betAmount)
+            object.eventResults.forEach(bet => totalProfit += Number(bet.profit))
+            const totalROI = totalProfit / totalBetAmount * 100
+            let trBg
+            let textColor
+            totalProfit >= 0 ? trBg = "bg-lime-200" : trBg = "bg-red-200"
+            totalProfit >= 0 ? textColor = "text-green-700" : textColor = "text-red-600"
+
             return (
-                <div key={object.event}>
-                    <p>{object.event}</p>
+                <table key={object.event} className="w-full mt-6">
+                    <caption className="text-left text-xl font-medium px-4 py-2">{object.event}</caption>
+                    <thead>
+                        <tr className="bg-slate-200">
+                            <th scope="col" className="text-left py-1 px-4">Matchup</th>
+                            <th scope="col" className="text-left">Pick</th>
+                            <th scope="col" className="text-left">Result</th>
+                            <th scope="col" className="text-left">Units Bet</th>
+                            <th scope="col" className="text-left">Units Profit</th>
+                            <th scope="col" className="text-left">ROI</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         {betResults}
+                        <tr className={trBg}>
+                            <th className="text-left py-1 px-4">Total</th>
+                            <th></th>
+                            <th></th>
+                            <th className="text-left">{totalBetAmount}</th>
+                            <th className={`text-left ${textColor}`}>{totalProfit}</th>
+                            <th className={`text-left ${textColor}`}>{totalROI.toFixed(0)}%</th>
+                        </tr>
                     </tbody>
-                </div>
+                </table>
             )
         })
         content = (
-            <div>
-                <p>{data.displayName}'s MMA Bet History</p>
+            <div className="w-full p-6 ">
+                <h3 className="bg-slate-600 text-white p-2 text-lg font-medium tracking-wide">{data.displayName}'s MMA Bet History</h3>
                 {betHistory}
             </div>
         )
