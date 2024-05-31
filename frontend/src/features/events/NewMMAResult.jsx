545c2bd25e0700c6e4528c2a0ac97215e4e9bc9a
Ray Mak <projectraymak@gmail.com>
2024-05-31 16:25:06 -0700

    Created useEventsWithNoResults hook
    This hook is used to find all MMA events that do not have results logged yet

--- frontend/src/features/events/NewMMAResult.jsx
+++ frontend/src/features/events/NewMMAResult.jsx
@@ -1,43 +1,23 @@
-import { useAddNewMMAResultMutation, useGetMMAResultsQuery } from "./mmaResultsApiSlice"
-import { useGetMMAEventsQuery } from "./mmaEventsApiSlice"
-import { useNavigate } from "react-router-dom"
+import useEventsWithNoResults from "./useEventsWithNoResults"
 import NewMMAResultForm from "./NewMMAResultForm"
 
 
 const NewMMAResult = () => {
-    const {
-        data: resultsData,
-        isLoading: resultsIsLoading,
-        isSuccess: resultsIsSuccess,
-        isError: resultsIsError,
-        error: resultsError
-    } = useGetMMAResultsQuery()
-
-    const {
-        data: eventsData,
-        isLoading: eventsIsLoading,
-        isSuccess: eventsIsSuccess,
-        isError: eventsIsError,
-        error: eventsError
-    } = useGetMMAEventsQuery()
-
-    const navigate = useNavigate()
+    const { eventsWithNoResults, isLoading, isError, errorMessage } = useEventsWithNoResults()
 
     let content
-    if (resultsIsLoading || eventsIsLoading) content = <p>Loading...</p>
-    if (resultsIsError || eventsIsError) content = <p>{eventsError?.data?.message}{resultsError?.data?.message}</p>
+    if (isLoading) content = <p>Loading...</p>
+    if (isError) content = <p>{errorMessage}</p>
+    console.log(eventsWithNoResults)
 
-    if (resultsIsSuccess && eventsIsSuccess) {
-        const resultsEntities = Object.values(resultsData.entities)
-        const eventsEntities = Object.values(eventsData.entities)
-        const resultsEventNames = resultsEntities.map(result => result.eventName)
-        const eventsWithNoResults = eventsEntities.filter(event => !resultsEventNames.includes(event.eventName))
-
+    if (eventsWithNoResults) {
         content = (
             <div>
                 <NewMMAResultForm events={eventsWithNoResults} />
             </div>
         )
+    } else {
+        content = <p>All events have been logged</p>
     }
 
     return content
---
+++ frontend/src/features/events/useEventsWithNoResults.jsx
@@ -0,0 +1,43 @@
+import { useGetMMAResultsQuery } from "./mmaResultsApiSlice"
+import { useGetMMAEventsQuery } from "./mmaEventsApiSlice"
+import { useMemo } from "react"
+
+const useEventsWithNoResults = () => {
+    const {
+        data: resultsData,
+        isLoading: resultsIsLoading,
+        isSuccess: resultsIsSuccess,
+        isError: resultsIsError,
+        error: resultsError
+    } = useGetMMAResultsQuery()
+
+    const {
+        data: eventsData,
+        isLoading: eventsIsLoading,
+        isSuccess: eventsIsSuccess,
+        isError: eventsIsError,
+        error: eventsError
+    } = useGetMMAEventsQuery()
+
+    const eventsWithNoResults = useMemo(() => {
+        if (resultsIsSuccess && eventsIsSuccess) {
+            const resultsEntities = Object.values(resultsData.entities)
+            const eventsEntities = Object.values(eventsData.entities)
+            const resultsEventNames = resultsEntities.map(result => result.eventName)
+            return eventsEntities.filter(event => !resultsEventNames.includes(event.eventName))
+        }
+        return []
+    }, [resultsData, eventsData, resultsIsSuccess, eventsIsSuccess])
+
+    const isLoading = resultsIsLoading || eventsIsLoading
+    const isError = resultsIsError || eventsIsError
+    const errorMessage = (eventsError?.data?.message || '') + (resultsError?.data?.message || '')
+    return {
+        eventsWithNoResults,
+        isLoading,
+        isError,
+        errorMessage
+    }
+}
+
+export default useEventsWithNoResults
