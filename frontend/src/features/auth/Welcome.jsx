4e89a8f48e3c9bffcbdefbfce97eeb83aa4ec9a3
Ray Mak <projectraymak@gmail.com>
2024-06-06 17:04:11 -0700

    Linked UserMMAEvents to Welcome page

--- frontend/src/features/auth/Welcome.jsx
+++ frontend/src/features/auth/Welcome.jsx
@@ -1,17 +1,48 @@
 import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
+import { useGetUserResultsQuery } from "../users/userMMAEventsApiSlice"
+import useAuth from "../../hooks/useAuth"
+import UserMMAEvents from "../users/UserMMAEvents"
+import UserUpcomingEvents from "../users/UserUpcomingEvents"
 
 const Welcome = () => {
+    const { id } = useAuth()
     const navigate = useNavigate()
 
-    return (
-        <div className="w-full flex justify-center">
-            <div className="w-4/5 flex flex-col gap-6 justify-center p-8 border-2 border-gray rounded-lg lg:w-3/5 2xl:w-1/2">
-                <p className="text-xl text-center">You do not have any upcoming picks</p>
-                <button onClick={() => navigate("/dash/addpick")} type="button" className="w-full bg-brightRed text-white h-10 font-medium rounded-md">Add pick</button>
+    const {
+        data,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUserResultsQuery({ id })
+    console.log(id)
+
+    let content
+
+    if (isLoading) content = <p>Loading...</p>
+
+    if (isError) {
+        content = <p>{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+        content = (
+            <div className="w-full flex flex-col items-center">
+                <div className="w-4/5 flex flex-col gap-6 justify-center p-8 border-2 border-gray rounded-lg lg:w-3/5 2xl:w-1/2">
+                    <div>
+                        <p className="text-xl text-center">You do not have any upcoming picks</p>
+                        <button onClick={() => navigate("/dash/addpick")} type="button" className="w-full bg-brightRed text-white h-10 font-medium rounded-md">Add pick</button>
+                    </div>
+                    <UserUpcomingEvents data={data} />
+                    <UserMMAEvents data={data} />
+                </div>
+
             </div>
-        </div>
-    )
+        )
+    }
+
+    return content
 }
 
 export default Welcome
--- frontend/src/features/users/UserMMAEvents.jsx
+++ frontend/src/features/users/UserMMAEvents.jsx
@@ -1,91 +1,74 @@
 //This displays a users upcoming and past MMA events that they have picks/bets on
 import { useGetUserResultsQuery } from "./userMMAEventsApiSlice"
 
-const UserMMAEvents = () => {
-    const {
-        data,
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    } = useGetUserResultsQuery({ id: "66328e6dd2d69e0a8f67a2c2" })
-
+const UserMMAEvents = ({ data }) => {
     console.log(data)
 
-    let content
-
-    if (isLoading) content = <p>Loading...</p>
-
-    if (isError) {
-        content = <p>{error?.data?.message}</p>
-    }
-
-    if (isSuccess) {
-        const betHistory = data.betHistory.map(object => {
-            const betResults = object.eventResults.map(bet => {
-                let textColor
-                if (bet.result === "Win") {
-                    textColor = "text-green-700"
-                } else if (bet.result === "Loss") {
-                    textColor = "text-red-600"
-                } else {
-                    textColor = "text-gray-500"
-                }
-                return (
-                    <tr key={bet._id}>
-                        <td className="px-4">{bet.matchup}</td>
-                        <td>{bet.pick}</td>
-                        <td className={textColor}>{bet.result}</td>
-                        <td>{bet.betAmount.toFixed(2)}</td>
-                        <td>{bet.profit}</td>
-                        <td>{bet.roi}%</td>
-                    </tr>
-                )
-            })
-            let totalBetAmount = 0
-            let totalProfit = 0
-            object.eventResults.forEach(bet => totalBetAmount += bet.betAmount)
-            object.eventResults.forEach(bet => totalProfit += Number(bet.profit))
-            const totalROI = totalProfit / totalBetAmount * 100
-            let trBg
+    const betHistory = data.betHistory.map(object => {
+        const betResults = object.eventResults.map(bet => {
             let textColor
-            totalProfit >= 0 ? trBg = "bg-lime-200" : trBg = "bg-red-200"
-            totalProfit >= 0 ? textColor = "text-green-700" : textColor = "text-red-600"
-
+            if (bet.result === "Win") {
+                textColor = "text-green-700"
+            } else if (bet.result === "Loss") {
+                textColor = "text-red-600"
+            } else {
+                textColor = "text-gray-500"
+            }
             return (
-                <table key={object.event} className="w-full mt-6 border-2 border-zinc-400">
-                    <caption className="text-left text-xl font-medium px-4 py-2">{object.event}</caption>
-                    <thead>
-                        <tr className="bg-slate-200">
-                            <th scope="col" className="text-left px-4">Matchup</th>
-                            <th scope="col" className="text-left">Pick</th>
-                            <th scope="col" className="text-left">Result</th>
-                            <th scope="col" className="text-left">Units Bet</th>
-                            <th scope="col" className="text-left">Units Profit</th>
-                            <th scope="col" className="text-left">ROI</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {betResults}
-                        <tr className={trBg}>
-                            <th className="text-left py-1 px-4">Total</th>
-                            <th></th>
-                            <th></th>
-                            <th className="text-left">{totalBetAmount}</th>
-                            <th className={`text-left ${textColor}`}>{totalProfit.toFixed(2)}</th>
-                            <th className={`text-left ${textColor}`}>{totalROI.toFixed(0)}%</th>
-                        </tr>
-                    </tbody>
-                </table>
+                <tr key={bet._id}>
+                    <td className="px-4">{bet.matchup}</td>
+                    <td>{bet.pick}</td>
+                    <td className={textColor}>{bet.result}</td>
+                    <td>{bet.betAmount.toFixed(2)}</td>
+                    <td>{bet.profit}</td>
+                    <td>{bet.roi}%</td>
+                </tr>
             )
         })
-        content = (
-            <div className="w-full p-6 ">
-                <h3 className="bg-slate-600 text-white p-2 text-lg font-medium tracking-wide">{data.displayName}'s MMA Bet History</h3>
-                {betHistory}
-            </div>
+        let totalBetAmount = 0
+        let totalProfit = 0
+        object.eventResults.forEach(bet => totalBetAmount += bet.betAmount)
+        object.eventResults.forEach(bet => totalProfit += Number(bet.profit))
+        const totalROI = totalProfit / totalBetAmount * 100
+        let trBg
+        let textColor
+        totalProfit >= 0 ? trBg = "bg-lime-200" : trBg = "bg-red-200"
+        totalProfit >= 0 ? textColor = "text-green-700" : textColor = "text-red-600"
+
+        return (
+            <table key={object.event} className="w-full mt-6 border-2 border-zinc-400">
+                <caption className="text-left text-xl font-medium px-4 py-2">{object.event}</caption>
+                <thead>
+                    <tr className="bg-slate-200">
+                        <th scope="col" className="text-left px-4">Matchup</th>
+                        <th scope="col" className="text-left">Pick</th>
+                        <th scope="col" className="text-left">Result</th>
+                        <th scope="col" className="text-left">Units Bet</th>
+                        <th scope="col" className="text-left">Units Profit</th>
+                        <th scope="col" className="text-left">ROI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {betResults}
+                    <tr className={trBg}>
+                        <th className="text-left py-1 px-4">Total</th>
+                        <th></th>
+                        <th></th>
+                        <th className="text-left">{totalBetAmount}</th>
+                        <th className={`text-left ${textColor}`}>{totalProfit.toFixed(2)}</th>
+                        <th className={`text-left ${textColor}`}>{totalROI.toFixed(0)}%</th>
+                    </tr>
+                </tbody>
+            </table>
         )
-    }
+    })
+    const content = (
+        <div className="w-full p-6 ">
+            <h3 className="bg-slate-600 text-white p-2 text-lg font-medium tracking-wide">{data.displayName}'s MMA Bet History</h3>
+            {betHistory}
+        </div>
+    )
+
 
     return content
 }
