29c0fd2a00970d64c573d27bed8161fd0fcdf87a
Ray Mak <projectraymak@gmail.com>
2024-06-25 12:43:45 -0700

    Updated LineChartMMA
    Added props, parlays, and totals to chart. Also added styling

--- frontend/src/components/LineChartMMA.jsx
+++ frontend/src/components/LineChartMMA.jsx
@@ -8,7 +8,9 @@ import {
     LineElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    plugins,
+    scales
 } from "chart.js"
 
 ChartJS.register(
@@ -24,7 +26,11 @@ ChartJS.register(
 const LineChartMMA = ({ data }) => {
     const labels = ["N/A"]
     const mlData = [0]
+    const propData = [0]
+    const parlayData = [0]
     let mlTotals = 0
+    let propTotals = 0
+    let parlayTotals = 0
 
     data.betHistory.toReversed().forEach(object => {
         labels.push(object.event)
@@ -34,23 +40,95 @@ const LineChartMMA = ({ data }) => {
         })
         mlData.push(mlTotals.toFixed(2))
 
+        object.propResults.forEach(matchup => {
+            let propProfit = Number(matchup.profit)
+            propTotals += propProfit
+        })
+        propData.push(propTotals.toFixed(2))
+
+        object.parlayResults.forEach(matchup => {
+            let parlayProfit = Number(matchup.profit)
+            parlayTotals += parlayProfit
+        })
+        parlayData.push(parlayTotals)
+
     })
 
-    const options = {}
+    const totalData = []
+    for (let i = 0; i < mlData.length; i++) {
+        const totalProfit = Number(mlData[i]) + Number(propData[i]) + Number(parlayData[i])
+        totalData.push(totalProfit)
+    }
+
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "bottom",
+            },
+            title: {
+                display: true,
+                text: "Units Profit Per Event Over Time",
+                font: {
+                    size: 18,
+                    weight: 600
+                }
+            }
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: "Events",
+                    font: {
+                        size: 14,
+                        weight: 600
+                    }
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: "Units Profit",
+                    font: {
+                        size: 14,
+                        weight: 600
+                    }
+                }
+            }
+        }
+    }
 
     const chartData = {
         labels,
         datasets: [
             {
                 label: "Moneyline Bets",
-                data: mlData
+                data: mlData,
+                borderColor: "#2991FF"
+            },
+            {
+                label: "Prop Bets",
+                data: propData,
+                borderColor: "#DF29FF"
+            },
+            {
+                label: "Parlays",
+                data: parlayData,
+                borderColor: "#F3DF36"
+            },
+            {
+                label: "Total Profit",
+                data: totalData,
+                borderColor: "#189C00"
             }
         ]
     }
 
-    console.log(labels, mlData)
+    console.log(totalData)
     return (
-        <Line options={options} data={chartData} />
+        <Line options={options} data={chartData} height={100} />
     )
 }
 
